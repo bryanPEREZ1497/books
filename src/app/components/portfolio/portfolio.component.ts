be0e2fc0ad9ad9c690672c81fdf6634fb0a0a153
Bryan Pérez <bam.perez@yavirac.edu.ec>
2022-08-12 11:57:59 -0500

    Final

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { fromEvent, merge } from 'rxjs';
 
 @Component({
   selector: 'app-portfolio',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
-  constructor() { }
+  skillItems = document.getElementsByClassName('skills__item');
+  openOrCloseModal = fromEvent(this.skillItems, 'click');
+
+  close = document.getElementsByClassName("close");
+  closeModal = fromEvent(this.close, 'click')
+
+
+
+  @ViewChild("myModal")
+  modal!: ElementRef;
+
+  @ViewChild("caption")
+  captionText!: ElementRef;
+  
+  @ViewChild("modalContent")
+  modalContent!: ElementRef;
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+    this.openOrCloseModal.subscribe(x => {
+
+      this.modal.nativeElement.style.display = 'block';
+
+      this.modalContent.nativeElement.innerHTML = x.target
+    });
+    
+    this.closeModal.subscribe(x => {
+      this.modal.nativeElement.style.display = 'none';
+    });
+
   }
+
 
 }
